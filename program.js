b73d4912aca1f5fdf79f89dfc763373ff1080b1e
valerie2405 <valerie.arobert@gmail.com>
2022-09-28 09:24:23 -0400

    set up buttons and placeholder for text

---
+++ program.js
@@ -0,0 +1,16 @@
+
+function buttonClickedMarks1(){
+    let Marks1Text = "text goes here";
+    
+    let buttonDiv = document.getElementById("button-div");
+
+    buttonDiv.innerHTML=Marks1Text;
+}
+
+function buttonClickedColor1(){
+    let Color1 = "text goes here"
+
+    let buttonDiv1 = document.getElementById("button-div1");
+
+    buttonDiv1.innerHTML=Color1;
+}
