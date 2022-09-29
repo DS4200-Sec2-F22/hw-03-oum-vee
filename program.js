5275b15d93332d0f0d4b63164fd1f4c7e9593c65
valerie2405 <valerie.arobert@gmail.com>
2022-09-29 09:44:36 -0400

    index and program

--- program.js
+++ program.js
@@ -1,10 +1,12 @@
+let buttonDiv = document.getElementById("button-div");
 
 function buttonClickedMarks1(){
-    let Marks1Text = "text goes here";
-    
-    let buttonDiv = document.getElementById("button-div");
 
-    buttonDiv.innerHTML=Marks1Text;
+    let Marks1Text = '<h3>Marks</h3> <ul> <li>Area</li> <li>Lines</li> </ul>';
+    let Channel = ' <h3>Channel</h3> <ul> <li>Color (Hue)</li> <ul><li>Represents the attribute of type Categorical.</li> <li>The first color is red representingvotes for Trump and the second color is blue representing the votes for Clinton</li></ul> <li>Size--> Area </li></ul>'
+    buttonDiv.style.display = 'hidden';
+    buttonDiv.innerHTML=Marks1Text + Channel;
+
 }
 
 function buttonClickedColor1(){
